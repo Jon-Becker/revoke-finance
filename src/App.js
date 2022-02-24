a4295daea0ed611c1d0006b016e0c04113ae86c0
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2022-02-24 17:59:36 -0500

    initial stable commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import styles from './styles.module.scss';
 import Sidebar from './components/sidebar';
 import Navbar from './components/navbar';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "./components/wallet/injector";
+import {useEagerConnect, web3} from "web3";
 
 function App() {
 
   const [activeDashboardView, setDashboardView] = useState("dashboard");
 
+  const { active, account, library, activate, deactivate } = useWeb3React()
+
+  async function connect() {
+    try {
+      await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      await deactivate()
+    } catch (ex) {
+      console.log(ex)
+    }
+    
+  }
+
   return (
     <section className={styles.main}>
       <Sidebar active={activeDashboardView} updateActive={setDashboardView} />
-      <Navbar />
+      <Navbar active={active} account={account} connect={connect} />
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from 'web3'
+
+function getLibrary(provider) {
+  return new Web3(provider)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    
-    <App />
-    
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
